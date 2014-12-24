26d25d88b73f82446ac6836cbc1d5409e8f533d8
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-12-24 11:55:49 -0800

    and rerun rake

--- out/2.14_small/install.R
+++ out/2.14_small/install.R
@@ -1,7 +1,7 @@
 # DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
 # use 'rake' to generate 'install.R'.
 
-url <- "http://bioconductor.org/packages/3.0/bioc"
+url <- "http://bioconductor.org/packages/2.14/bioc"
 
 install.packages("BiocInstaller", repos=url)
 

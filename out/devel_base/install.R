9acb54366d0770ae98f48127e8e7ba3d1604aed6
Lori Shepherd <shepherl@gmail.com>
2017-01-09 12:35:22 -0500

    Update to use Bioconductor repos and debug devel_base
      - fix warnings from require
      - add check for valid/out dated packages

--- out/devel_base/install.R
+++ out/devel_base/install.R
@@ -6,11 +6,12 @@ url <- "http://bioconductor.org/packages/3.5/bioc"
 if ("BiocInstaller" %in% rownames(installed.packages()))
 	remove.packages("BiocInstaller")
 
-
 install.packages("BiocInstaller", repos=url)
 
 builtins <- c("Matrix", "KernSmooth", "mgcv")
 
 for (builtin in builtins)
-    if(!do.call(require, list(builtin)))
+    if (!suppressWarnings(require(builtin, character.only=TRUE)))
         BiocInstaller::biocLite(builtin)
+
+suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))

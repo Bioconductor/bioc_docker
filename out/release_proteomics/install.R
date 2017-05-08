69d3b5c566a9e17f601e58baf9d5e46ca8d64508
Lori Shepherd <shepherl@gmail.com>
2017-05-08 11:36:06 -0400

    raked to add out directories for new mscore and proteomics dockers

---
+++ out/release_proteomics/install.R
@@ -0,0 +1,49 @@
+# DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
+# use 'rake' to generate 'install.R'.
+
+library(BiocInstaller) # shouldn't be necessary
+
+
+## Obtain list of packages in view, as defined in config.yml
+
+wantedBiocViews <- c("Proteomics","MassSpectrometryData")
+
+
+## software packages
+con1 <- url("http://www.bioconductor.org/packages/3.5/bioc/VIEWS")
+dcf1 <- as.data.frame(read.dcf(con1), stringsAsFactors=FALSE)
+## data packages
+con2 <- url("http://www.bioconductor.org/packages/3.5/data/experiment/VIEWS")
+dcf2 <- as.data.frame(read.dcf(con2), stringsAsFactors=FALSE)
+
+dcf <- rbind(dcf1[, c("Package", "biocViews")],
+             dcf2[, c("Package", "biocViews")])
+
+i <- lapply(wantedBiocViews, grep, dcf$biocViews)
+pkgs_matching_views <- dcf$Package[unique(unlist(i))]
+
+ap.db <- available.packages(contrib.url(biocinstallRepos()))
+ap <- rownames(ap.db)
+
+##
+## Selection and fine-tuning of packages to install
+##
+pkgs_to_install <- pkgs_matching_views[pkgs_matching_views %in% ap]
+
+# don't reinstall anything that's installed already
+pkgs_to_install <- setdiff(pkgs_to_install, rownames(installed.packages()))
+
+## Start the actual installation:
+biocLite(pkgs_to_install)
+
+# just in case there were warnings, we want to see them
+# without having to scroll up:
+warnings()
+
+if (!is.null(warnings())) {
+    w <- capture.output(warnings())
+    if (length(grep("is not available|had non-zero exit status", w)))
+        quit("no", 1L)
+}
+
+suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))

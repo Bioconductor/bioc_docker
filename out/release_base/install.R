235df28b1a0b74024ddbfee6cbcf7946fb37d488
lshep <shepherl@gmail.com>
2017-11-01 11:57:47 -0400

    Update for release 3.6 and devel 3.7

--- out/release_base/install.R
+++ out/release_base/install.R
@@ -1,7 +1,7 @@
 # DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
 # use 'rake' to generate 'install.R'.
 
-url <- "http://bioconductor.org/packages/3.5/bioc"
+url <- "http://bioconductor.org/packages/3.6/bioc"
 
 if ("BiocInstaller" %in% rownames(installed.packages()))
 	remove.packages("BiocInstaller")

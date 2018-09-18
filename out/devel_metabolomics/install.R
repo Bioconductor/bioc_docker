bbb1094266e78ab58fe713c8f96e7b8fd131785e
Steffen Neumann <sneumann@ipb-halle.de>
2018-09-18 08:04:30 +0200

    Disable mQTL.NMR, closing #58

--- out/devel_metabolomics/install.R
+++ out/devel_metabolomics/install.R
@@ -1,8 +1,6 @@
 # DO NOT EDIT 'install.R'; instead, edit 'install.R.in' and
 # use 'rake' to generate 'install.R'.
 
-#library(BiocInstaller) # shouldn't be necessary
-
 ##
 ## Obtain list of packages in view, as defined in config.yml
 ##
@@ -30,7 +28,6 @@ for (i in 1:nrow(dcf))
 }
 
 ap.db <- available.packages(contrib.url(BiocManager::repositories()))
-#ap.db <- available.packages(contrib.url(biocinstallRepos()))
 ap <- rownames(ap.db)
 
 ##
@@ -41,8 +38,12 @@ pkgs_to_install <- pkgs_matching_views[pkgs_matching_views %in% ap]
 # don't reinstall anything that's installed already
 pkgs_to_install <- setdiff(pkgs_to_install, rownames(installed.packages()))
 
+# Explicitly disable broken packages:
+
+# https://github.com/Bioconductor/bioc_docker/issues/58
+pkgs_to_install <- pkgs_to_install[!grepl("mQTL.NMR", pkgs_to_install)]
+
 ## Start the actual installation:
-#biocLite(pkgs_to_install)
 BiocManager::install(pkgs_to_install, update=FALSE, ask=FALSE)
 
 # just in case there were warnings, we want to see them
@@ -56,5 +57,4 @@ if (!is.null(warnings()))
         quit("no", 1L)
 }
 
-#suppressWarnings(BiocInstaller::biocValid(fix=TRUE, ask=FALSE))
 suppressWarnings(BiocManager::install(update=TRUE, ask=FALSE))
